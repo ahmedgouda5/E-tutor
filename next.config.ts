afb8e14dbc9366386a78140ec8256ad7950e3a7a
ahmedgouda5 <ahmedgodayyy@gmail.com>
2025-11-19 13:31:56 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images: {
+    unoptimized: false,  // خلّيه false عشان optimization يشتغل
+    // domains: []  // مش محتاج لو كل الصور local
+  },
 };
 
 export default nextConfig;
